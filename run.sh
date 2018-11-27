7b1ca504fbefd0f6382a6cc9f1c8d4a26f44726c
Oliver Moser <oliver.moser@codecentric.de>
2018-11-27 08:39:47 +0100

    added data

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git pull
-docker run -p 127.0.0.1:8888:8888 -v $(pwd)/notebooks:/notebooks codecentric.ai-docker
+docker run -p 127.0.0.1:8888:8888 -v $(pwd)/notebooks:/notebooks -v $(pwd)/data:/data codecentric.ai-docker
