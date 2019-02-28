77c4eaf3396948b6db1eebb23423747ae65d4b6c
Paul Strobel <github@paulstrobel.de>
2019-02-28 11:18:24 +0100

    fixed wrong docker imagename

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 git pull
-docker run -p 127.0.0.1:8888:8888 -v "%cd%/notebooks":/notebooks -v "%cd%/data":/data codecentric.ai-docker
+docker run -p 127.0.0.1:8888:8888 -v "%cd%/notebooks":/notebooks -v "%cd%/data":/data codecentric/codecentric.ai-docker
